0fb8faaa780b2769cb3991de40904815ed213fa1
David Sly <davesly@mac.com>
2020-02-02 16:03:28 -0500

    Initial Commit

---
+++ AppcodaGitTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppcodaGitTutorial
+//
+//  Created by Dave on 2/2/20.
+//  Copyright © 2020 Dave. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
